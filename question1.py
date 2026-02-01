2efe1a05567f4c3e60712bb060590be23387c94f
ankitgujre <ankitgujre374@gmail.com>
2026-02-01 10:09:13 +0530

    printing characters with index

--- question1.py
+++ question1.py
@@ -1,7 +1,8 @@
-# st = "Welcome to the python string"
-# for ch in st:
-#     print(ch, end=" ")
+st = "Welcome to the python string"
+n = len(st)
+for ch in range(n):
+    print(ch, "=", st[ch])
 
-a = range(5)
-for i in a:
-    print(i)
+# a = range(5)
+# for i in a:
+#     print(i)
