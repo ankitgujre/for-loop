06be6ed17811da40f5fd0191c08dc2110535ec79
ankitgujre <ankitgujre374@gmail.com>
2026-02-02 11:28:23 +0530

    count digit in string using isdigit() function and without function

---
+++ countDigits.py
@@ -0,0 +1,19 @@
+# st = "py12th0n99"
+# count = 0
+
+# for ch in st:
+#     if ch >= '0' and ch <= '9':
+#         count += 1
+
+# print(count)
+
+
+st = "py12th0n99012"
+count = 0
+
+for ch in st:
+    if ch.isdigit():
+        count += 1
+
+print(count)
+
